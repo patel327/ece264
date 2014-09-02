33938e30bb1516249dad45215d75a999c47c6653
patel327 <patel327@purdue.edu>
2014-09-02 00:34:04 -0400

    Update answer01.c

--- PA01/answer01.c
+++ PA01/answer01.c
@@ -81,8 +81,8 @@ int arrayIndexRFind(int needle, const int * haystack, int len)
 int arrayFindSmallest(int * array, int len)
 {
     int ind;
-    int answer=0;
-    int test;
+    int answer=len-1;
+    int small = array[len-1];
     
     if(len==0)
     {    
@@ -92,9 +92,13 @@ int arrayFindSmallest(int * array, int len)
     {
         for(ind= len - 1;ind>=0;ind--)
         {
-            
+            if(array[ind]<small)
+            {
+                small =array[ind];
+                answer=ind;
+            }
         }
     }
-    
-    return 0;
+    return answer;
+    //return 0;
 }
