1362885122ad2f897c93491a7387b801ff744eec
patel327 <patel327@purdue.edu>
2014-09-10 14:47:12 -0400

    Update pa02.c

--- PA02/pa02.c
+++ PA02/pa02.c
@@ -61,12 +61,13 @@ int main(int argc, char * * argv)
     // -- my_strcat. You will have to do this yourself... just
     // look at my_strcpy for an example, and go from there.
    
-    my_strcat(buffer, "Hello");
-    printf("my_strcat(buffer, \"%s\"), buffer = \"%s\"\n", s1, buffer); //get doubles?
+    char buffer2[BUFFER_LEN];
+    my_strcat(buffer2, s1);
+    printf("my_strcat(buffer2, \"%s\"), buffer2 = \"%s\"\n", s1, buffer2); //get doubles?
     my_strcat(buffer, s2);
-    printf("my_strcat(buffer, \"%s\"), buffer = \"%s\"\n", s2, buffer);
+    printf("my_strcat(buffer2, \"%s\"), buffer2 = \"%s\"\n", s2, buffer);
     my_strcat(buffer, s3);
-    printf("my_strcat(buffer, \"%s\"), buffer = \"%s\"\n", s3, buffer);
+    printf("my_strcat(buffer2, \"%s\"), buffer2 = \"%s\"\n", s3, buffer);
 
     // -- my_isspace. You will have to do this for yourself.
 
