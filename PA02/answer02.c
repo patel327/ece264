bac20199f471e668480d4820dc7d4ead586854f0
patel327 <patel327@purdue.edu>
2014-09-10 14:31:16 -0400

    Update answer02.c

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -137,5 +137,7 @@ char * my_strcat(char * dest, const char * src)
     dest[counter+1+ind] = src[ind];
     ind++;
   }
-  dest[counter+1+ind] = '\0'
+  dest[counter+1+ind] = '\0';
+  
+  return &dest[0];
 }
