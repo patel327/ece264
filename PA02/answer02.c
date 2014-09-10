76352e87a396a3756f5ff28ab174e2297d56b454
patel327 <patel327@purdue.edu>
2014-09-09 21:53:39 -0400

    Update answer02.c

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -3,5 +3,35 @@
 
 size_t my_strlen(const char * str);
 {
+  int len=0;
   
+  while(str[len] != '\0')
+  {  
+    len = len + 1;
+  }
+  return len;
 }
+
+int my_countchar(const char * str, char ch);
+{
+  int len = 0;
+  int counter = 0;
+  int occur = 0;
+  
+  while(str[len] != '\0')
+  {
+    len = len + 1;
+  }
+  
+  do
+  {
+    if(str[counter] == 'ch')
+    {
+      occur = occur + 1;
+    }
+    counter = counter + 1;
+  }while(counter<len+1);
+  
+  return occur;
+}
+
