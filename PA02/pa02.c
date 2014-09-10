f6dcce37a96dd5c6e7dd247da5a1efda5af75b12
patel327 <patel327@purdue.edu>
2014-09-10 11:52:53 -0400

    Update pa02.c

--- PA02/pa02.c
+++ PA02/pa02.c
@@ -39,9 +39,9 @@ int main(int argc, char * * argv)
     printf("my_strchr(\"%s\", 'o') = %s\n", s2, my_strchr(s2, 'o'));
     printf("my_strchr(\"%s\", '\\0') = %s\n", s3, my_strchr(s3, '\0'));
     
-    printf("my_strrchr(\"%s\", 'l') = %s\n", s1, my_strchr(s1, 'l')); //ld
-    printf("my_strrchr(\"%s\", 'o') = %s\n", s2, my_strchr(s2, 'o')); //null
-    printf("my_strrchr(\"%s\", '\\0') = %s\n", s3, my_strchr(s3, '\0')); // 
+    printf("my_strrchr(\"%s\", 'l') = %s\n", s1, my_strrchr(s1, 'l')); //ld
+    printf("my_strrchr(\"%s\", 'o') = %s\n", s2, my_strrchr(s2, 'o')); //null
+    printf("my_strrchr(\"%s\", '\\0') = %s\n", s3, my_strrchr(s3, '\0')); // 
 
     // -- my_strstr, should be: "World!", "Hello World!", "(null)"
     //printf("my_strstr(\"%s\", \"World\") = %s\n", s1, my_strstr(s1, "World"));
