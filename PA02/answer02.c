787c7516a3e08c7acebdbf7e5c399f0365fa4b70
patel327 <patel327@purdue.edu>
2014-09-09 23:24:24 -0400

    Update answer02.c

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -40,10 +40,10 @@ char * my_strchr(const char * str, int ch)
   int len = 0;
   int place = -2;
   int counter = 0;
-  const char * newstr;
+  char * newstr;
   int ind;
-  const char * rtrn;
-  const char * null = [(null)];
+  char * rtrn;
+  char * null = [(null)];
   
   while(str[len] != '\0')
   {
