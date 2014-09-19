f6102f6fc999146577e45e2cb66df5772079605f
patel327 <patel327@purdue.edu>
2014-09-19 14:13:38 -0400

    Update answer03.c

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -73,9 +73,9 @@ char ** explode(const char * str, const char * delims, int * arrLen)
     ind++;
     arrstr[row] = malloc(sizeof(char)*numcol);
     memcpy(arrstr[row],str,(ind-begin));
-    arrstr[row][ind-begin] = '\0'
+    arrstr[row][ind-begin] = '\0';
   }
-  return *arrstr;
+  return **arrstr;
 }
 
 char * implode(char * * strArr, int len, const char * glue)
