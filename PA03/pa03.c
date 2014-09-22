599bbfaa2b5490cd8a23bf8a2ed18983862a0c25
patel327 <patel327@purdue.edu>
2014-09-22 18:48:41 -0400

    Update pa03.c

--- PA03/pa03.c
+++ PA03/pa03.c
@@ -14,11 +14,15 @@ char* src = "added";
 int n = 1;
 char * str = "The\nTuring test";
 char * delims = " \n";
+char * str1 = "The Turing test";
+char * delims1 = " ";
 int arrLen;
+int arrLen1
 
 
 printf("answer = %s\n", strcat_ex(&dest, &n, src));
 printf("answer2 = %s\n", explode(str, delims, &arrLen));
+printf("answer3 = %s\n", explode(str1, delims1, &arrLen1));
 
 return EXIT_SUCCESS;
 }
