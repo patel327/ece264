c0c873503855121397807ee66cd938ccee901dd7
Sneh B Patel <patel327@ee207lnx07.ecn.purdue.edu>
2014-09-29 09:46:46 -0400

    update pa03

--- PA03/pa03.c
+++ PA03/pa03.c
@@ -9,7 +9,7 @@ void test5()
   const char * delims = " \n";
   int n;
   char * * strArr = explode(str, delims, &n);
-  printf("answer2 = %s\n", explode(str, delims, &n));
+  printf("answer2 = %s\n", strArr[0]);
 }
 
 int main(int argc, char * * argv)
@@ -19,22 +19,22 @@ int main(int argc, char * * argv)
 //char * s1 = "Hello World!";
 //char * s2 = "";
 //char * s3 = "foo";
-char* dest = NULL;
-char* src = "added";
-int n = 1;
-char * str = "The\nTuring test";
-char * delims = " \n";
-char * str1 = "The Turing test";
-char * delims1 = " ";
-int arrLen;
-int arrLen1;
+//char* dest = NULL;
+//char* src = "added";
+//int n = 1;
+//char * str = "The\nTuring test";
+//char * delims = " \n";
+//char * str1 = "The Turing test";
+//char * delims1 = " ";
+//int arrLen;
+//int arrLen1;
 
- int arr[] = {1 ,2, 3};
+  //int arr[] = {1 ,2, 3};
 
 
-printf("answer = %s\n", strcat_ex(&dest, &n, src));
-printf("answer2 = %s\n", explode(str, delims, &arrLen));
-printf("answer3 = %s\n", explode(str1, delims1, &arrLen1));
+ //printf("answer = %s\n", strcat_ex(&dest, &n, src));
+ //printf("answer2 = %s\n", explode(str, delims, &arrLen));
+ //printf("answer3 = %s\n", explode(str1, delims1, &arrLen1));
 
 return EXIT_SUCCESS;
 }
