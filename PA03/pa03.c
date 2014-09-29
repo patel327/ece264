de86c1b2afaac25b69bab4826dd904745a95af9c
patel327 <patel327@purdue.edu>
2014-09-29 17:29:10 -0400

    Update pa03.c

--- PA03/pa03.c
+++ PA03/pa03.c
@@ -15,7 +15,7 @@ void test5()
 int main(int argc, char * * argv)
 {
   test5();
-  int len;
+  int len = 4;
   char * * strArr = explode("100 224 147 80", " ", &len);
   char * str = implode(strArr, len, ", ");
   printf("(%s)\n", str); // (100, 224, 147, 80)
