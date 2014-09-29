94b3a02ff690707a7eeda113d8cc6bec2b042aa4
patel327 <patel327@purdue.edu>
2014-09-29 10:09:41 -0400

    Update answer03.c

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -86,6 +86,7 @@ char * implode(char * * strArr, int len, const char * glue)
   int ind = 0;
   char* str = malloc(sizeof(char)*strlen(strArr[0]));
   str[0]='\0';
+  printf("(not dumped)\n");
   for(ind = 0; ind <= len - 1; ind++)
   {
     strcat_ex(&str, &len,strArr[ind]);
@@ -93,6 +94,7 @@ char * implode(char * * strArr, int len, const char * glue)
   }
 
   strcat_ex(&str, &len, strArr[ind]);
+  printf("(not dumped2)\n");
   return(&str[0]);
 }
 
