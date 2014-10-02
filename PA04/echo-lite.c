1c442b8c60291439c6fe13ff254a53613defdd3b
patel327 <patel327@purdue.edu>
2014-10-02 19:34:55 -0400

    Update echo-lite.c

--- PA04/echo-lite.c
+++ PA04/echo-lite.c
@@ -1,1 +1,17 @@
-//test
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * * argv)
+{
+    int ind;
+	
+    //printf("Printing command-line arguments:\n");
+    for(ind = 1; ind < argc; ind++) 
+    {
+	  printf("%s ", ind, argv[ind]);
+    }
+    //printf("Done!\n");
+
+    return EXIT_SUCCESS;
+}
