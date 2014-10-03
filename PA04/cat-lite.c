1c54c97d39d207fedb0248191fed387b57e8957d
patel327 <patel327@purdue.edu>
2014-10-03 10:31:35 -0400

    Update cat-lite.c

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -4,6 +4,22 @@
 
 int main(int argc, char * * argv)
 {
-  //code
+  int ind;
+    //printf("Printing command-line arguments:\n");
+  for(ind = 1; ind < argc; ind++)
+  {
+  if(strcmp(argv[ind],"--help") == 0)
+  {
+    printf("Usage: cat-lite [--help] [FILE]...\n"
+    "With no FILE, or when FILE is -, read standard input.\n\n"
+
+    "Examples:\n"
+    "cat-lite README   Print the file README to standard output.\n"
+    "cat-lite f - g    Print f's contents, then standard input,\n" 
+    "                  then g's contents.\n"
+    "cat-lite          Copy standard input to standard output.)\n"
+    return EXIT_SUCCESS;
+  }
+  }  
   
 }  
