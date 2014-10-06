60ba8682ef033c3fcd461350e15d4c48ac8d317a
patel327 <patel327@purdue.edu>
2014-10-06 12:14:53 -0400

    Update answer05.c

--- PA05/answer05.c
+++ PA05/answer05.c
@@ -1,1 +1,12 @@
-//test
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "answer05.h"
+
+void partitionAll(int value)
+{
+  int lineNo;
+  
+  lineNo = pow(2, value - 1)
+  
+}
