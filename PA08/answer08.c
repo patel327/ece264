a6c96786edcd6157387c4a12695df6896d69a9fc
patel327 <patel327@purdue.edu>
2014-10-31 15:29:21 -0400

    Update answer08.c

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -4,6 +4,8 @@
 #include <libgen.h>
 #include "answer08.h"
 
+int comparefs(const void *arg1, const void *arg2);
+
 List * List_createNode(const char * str){
   List * head = malloc(sizeof(List));
   head -> next = NULL;
@@ -33,6 +35,39 @@ int List_length(List * list){
   return counter;
 }
 
-List * List_merge(List * lhs, List * rhs, int (*compar)(const char *, const char*)){
-  
+List * List_merge(List * lhs, List * rhs, comparefs){
+  List* newlist = NULL;
+  while(lhs != NULL && rhs != NULL){
+    int determine = comparefs(lhs -> str, rhs -> str);
+    if(determine >= 0){
+      newlist -> str = lhs -> str;
+      lhs = lhs->next;
+    }
+    else{
+      newlist -> str = rhs -> str
+      rhs = rhs -> next
+    }
+    newlist -> next = NULL;
+  }
+  while(lhs != NULL){
+    newlist -> str = lhs -> str;
+    lhs = lhs -> next;
+    newlist -> next = NULL;
+  }
+  while(rhs != NULL){
+    newlist -> str = rhs -> str;
+    rhs = rhs -> next;
+    newlist -> next = NULL;
+  }
+  return newlist;
+}
+
+
+int comparefs(const void *arg1, const void *arg2)
+{
+  const char * const * ptr1 = (const char **) arg1;
+  const char * const * ptr2 = (const char **) arg2;
+  const char * str1 = * ptr1;
+  const char * str2 = * ptr2;
+  return(strcmp(str1, str2));
 }
