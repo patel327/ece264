1c7094adec5ed59dcf016380b09da4b451f81cfd
patel327 <patel327@purdue.edu>
2014-10-31 11:24:42 -0400

    Update answer08.c

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -1,1 +1,13 @@
-//Kappa
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <libgen.h>
+#include "answer08.h"
+
+List * List_createNode(const char * str){
+  List * head = malloc(sizeof(List));
+  head -> next = NULL;
+  head -> str = strdup(str)
+  
+  return head;
+}
