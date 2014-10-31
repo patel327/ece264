8c47d6f9e9461b7ab3491b027bbe4059c90a3d64
patel327 <patel327@purdue.edu>
2014-10-31 12:40:16 -0400

    Update answer08.c

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -11,3 +11,28 @@ List * List_createNode(const char * str){
   
   return head;
 }
+
+void List_destroy(List * list){
+  while(list != NULL){
+    List* newhead = list -> next;
+    free(list -> str);
+    free(list);
+    list = newhead;
+  }
+}
+
+int List_length(List * list){
+  if(list == NULL){
+    return 0;
+  }
+  int counter = 1;
+  while(list -> nest != NULL){
+    counter++;
+    list = list.next;
+  }
+  return counter;
+}
+
+List * List_merge(List * lhs, List * rhs, int (*compar)(const char *, const char*)){
+  
+}
