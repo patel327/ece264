4f3ab7069f5afdaab6a39f07572bac1225861661
patel327 <patel327@purdue.edu>
2014-11-02 15:23:52 -0500

    Update answer08.c

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -86,8 +86,8 @@ List * List_sort(List * list,int (*compar)(const char *arg1, const char *arg2)){
   List * front;
   List * back;
   List_split(list,&front,&back);
-  List_sort(front, compar);
-  List_sort(back, compar);
+  front = List_sort(front, compar);
+  back = List_sort(back, compar);
   list = List_merge(front,back,compar);
   return list;
 }
