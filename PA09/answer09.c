f62648197ad0da996d8408d53ce4788085e3f91b
patel327 <patel327@purdue.edu>
2014-11-07 10:16:25 -0500

    Update answer09.c

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -14,3 +14,23 @@ create_node(char * stars, char * name, char * address){
   node -> address = NULL;
   return node;
 }
+
+BusinessNode *
+tree_insert(BusinessNode * node, BusinessNode * root){
+  
+}
+
+BusinessNode *
+load_tree_from_file(char * filename){
+  
+}
+
+BusinessNode *
+tree_search_name(char * name, BusinessNode * root){
+  
+}
+
+void
+destroy_tree(BusinessNode * root){
+  
+}
