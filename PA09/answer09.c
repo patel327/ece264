3923a25b56b5b113302dfa57f67d7dde093e1c78
patel327 <patel327@purdue.edu>
2014-11-07 10:14:10 -0500

    Update answer09.c

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -1,1 +1,16 @@
-//kappa
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "answer09.h"
+
+BusinessNode *
+create_node(char * stars, char * name, char * address){
+  BusinessNode * node;
+  node = malloc(sizeof(BusinessNode));
+  node -> left = NULL;
+  node -> right = NULL;
+  node -> name = NULL;
+  node -> stars = NULL;
+  node -> address = NULL;
+  return node;
+}
