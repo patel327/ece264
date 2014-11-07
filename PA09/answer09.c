9be716854444a75890201136790c79ea814a74ad
patel327 <patel327@purdue.edu>
2014-11-07 11:41:11 -0500

    Update answer09.c

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -34,3 +34,40 @@ void
 destroy_tree(BusinessNode * root){
   
 }
+
+
+char ** explode(const char * str, const char * delims, int * arrLen)
+{
+  int ind;
+  int N = 0;
+  for(ind=0; ind<strlen(str);ind++)
+    {
+      if(strchr(delims, str[ind]) != NULL)
+        {
+          N++;
+        }
+    }
+  char**arrstr = malloc(sizeof(char*)*(N+1));
+  *arrLen = N + 1;
+  //int row;
+  ind = 0;
+  int last = 0;
+  int arrInd = 0;
+  for(ind=0; ind < strlen(str); ind++)
+  {
+    if(strchr(delims, str[ind]) != NULL)
+      {
+        arrstr[arrInd] = malloc(sizeof(char)*(ind-last + 1));
+        //arrstr[arrInd] = '\0';
+        memcpy(arrstr[arrInd], &str[last],ind-last);
+        arrstr[arrInd][ind - last] = '\0';
+        last = ind +1;
+        arrInd++;
+      }
+  }
+  arrstr[N] = malloc(sizeof(char)*(strlen(str)-last + 1));
+  //arrstr[N] = '\0';
+  memcpy(arrstr[N], &str[last],strlen(str)-last);
+  arrstr[N][strlen(str) - last] = '\0';
+  return (arrstr);
+}
