ee3619bdb74788e3cf43128d5681eb19395b8e6f
patel327 <patel327@purdue.edu>
2014-11-08 18:48:30 -0500

    Update answer09.c

--- PA09/answer09.c
+++ PA09/answer09.c
@@ -78,7 +78,7 @@ tree_search_name(char * name, BusinessNode * root){
   if((strcmp(name, root -> name)) > 0){
     root = tree_search_name(name, root -> right);
   }
-  if(root -> right != NULL){
+  if(root -> left != NULL){
   //else{
   //if((strcmp(name, root -> name)) > 0){
     root = tree_search_name(name, root -> left);
