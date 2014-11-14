a580cf5cee3040be93d341458bd171bd9470db25
patel327 <patel327@purdue.edu>
2014-11-13 19:32:47 -0500

    Update answer10.c

--- PA10/answer10.c
+++ PA10/answer10.c
@@ -1,1 +1,23 @@
-//Kappa
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "answer10.h"
+
+
+struct YelpDataBST* create_business_bst(const char* businesses_path,
+const char* reviews_path){
+  
+}
+
+struct Business* get_business_reviews(struct YelpDataBST* bst,
+char* name, char* state, char* zip_code){
+  
+}
+
+void destroy_business_bst(struct YelpDataBST* bst){
+  
+}
+
+void destroy_business_result(struct Business* b){
+  
+}
