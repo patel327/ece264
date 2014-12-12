093fef5315cfc11a761af06bb4c8961b031a1b36
patel327 <patel327@purdue.edu>
2014-12-12 18:55:00 -0500

    Update answer12.c

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -111,8 +111,8 @@ int primalityTestParallel(uint128 value, int n_threads){
 }
 
 void * is_prime(void * params){
-  params = (thread *) params;
-  if((params.value) % 2 == 0){
+  thread * params = (thread *) params;
+  if((params -> value) % 2 == 0){
     if(params -> value == 2){
       params -> prime = 1;
       return NULL;
