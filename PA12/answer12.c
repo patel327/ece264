c2f346580d07b4cc04c91b6856c21bfb35ffeea2
patel327 <patel327@purdue.edu>
2014-12-12 18:11:38 -0500

    Update answer12.c

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -4,6 +4,13 @@
 #include <pthread.h>
 #include "answer12.h"
 
+typedef struct thread_s{
+  uint128 lower;
+  uint128 higher;
+  uint128 value;
+  uint128 prime;
+}thread;
+
 uint128 alphaTou128(const char * str){
   uint128 ret = 0;
   int ind = 0;
@@ -53,5 +60,60 @@ char * u128ToString(uint128 value){
 }
 
 int primalityTestParallel(uint128 value, int n_threads){
+  long int max = floor(sqrt(value));
+  int size = max/n_threads;
+  pthread_t * arrThr = malloc(sizeof(pthread_t) * n_threads);
+  thread * ranges = malloc(sizeof(thread) * n_threads);
+  int count;
+  for(count = 0; count < n_threads; n_threads++){
+    ranges[count].value = value;
+    if(count == 0){
+    ranges[count].lower = 3;
+    }
+    else{
+      ranges[count].lower = count * size;
+    }
+    ranges[count].higher = (count+1) * size; 
+    pthread_create(arrThr[count],NULL, is_prime, ranges[count])
+  }  
+    
   
+//  if(value % 2 == 0){
+//    if(value == 2){
+//      return 1;
+//    }
+//    else{
+//      return 0;
+//    }
+//  }
+//  //long int max = floor(sqrt(value));
+//  long int i;
+//  for(i = 3; i <= max; ind += 2){
+//    if (n % ((2 * i) + 1) == 0) 
+//	  return 0;
+//  }
+//  return 1;
+}
+
+void * is_prime(void * params){
+  params = (thread *) params;
+  if((params -> value) % 2 == 0){
+    if(params -> value == 2){
+      params -> prime = 1;
+      return NULL;
+    }
+    else{
+      params -> prime = 0
+      return NULL;
+    }
+  }
+  //long int max = floor(sqrt(value));
+  long int i;
+  for(i = params -> lower; i <= (params -> upper); ind += 2){
+    if (params -> value % ((2 * i) + 1) == 0) 
+	  params -> prime = 0
+	  return NULL;
+  }
+  params -> prime = 1
+  return NULL;
 }
