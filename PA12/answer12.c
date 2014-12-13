adf43a2c534259c5b7a1fe4f771f2d075a54598e
patel327 <patel327@purdue.edu>
2014-12-12 21:08:10 -0500

    Update answer12.c

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -85,12 +85,12 @@ int primalityTestParallel(uint128 value, int n_threads){
     	ranges[count].lower = 3;
   }
     else{
-    	ranges[count].lower = count * size;
+    	ranges[count].lower = count + size;
     	if(ranges[count].lower % 2 ==0){
     		ranges[count].lower -= 1;
     	}
   }
-  	ranges[count].higher = (count+1) * size;
+  	ranges[count].higher = (count+1) + size;
   	if(ranges[count].higher % 2 ==0){
   		ranges[count].higher += 1;
   	}
